1f0fd0bc7d7698465562c98f62903912e7bd974b
Julien Louis <julienlouis.becode@gmail.com>
2021-05-14 14:49:38 +0200

    update

--- wordpress/wp-content/themes/dev-restaurant/header.php
+++ wordpress/wp-content/themes/dev-restaurant/header.php
@@ -10,3 +10,12 @@
 </head>
 
 <body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
