e4f4e38c25b2beb8faeeff266a19bee6f92d37d6
Julien Louis <julienlouis.becode@gmail.com>
2021-05-14 10:42:31 +0200

    save

---
+++ wordpress/wp-content/themes/dev-restaurant/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+function dev_restaurant_supports()
+{
+    add_theme_support('title_tag');
+}
+
+function dev_restaurant_register_assets()
+{
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap');
+}
+
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'dev_restaurant_register_assets');
---
+++ wordpress/wp-content/themes/dev-restaurant/index.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+
+
+Hello World
+
+<? get_footer() ?>
