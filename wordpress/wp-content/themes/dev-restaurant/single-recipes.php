cfbb74a053c8f3b7076e404dad5b2760945e3108
Julien Louis <julienlouis.becode@gmail.com>
2021-05-24 18:53:32 +0200

    weekend update

--- wordpress/wp-content/themes/dev-restaurant/single-recipes.php
+++ wordpress/wp-content/themes/dev-restaurant/single-recipes.php
@@ -4,17 +4,10 @@
 ?>
 <?php get_header() ?>
 
-<?php wp_link_pages(); ?>
 
 <h1>Single recipes test</h1>
 
 
-<?php if (have_posts()) : while (have_posts()) : the_post() ?>
-                <h1> <?php the_title() ?></h1>
-                <?php the_content() ?>
-
-<?php endwhile;
-endif ?>
 
 
 
