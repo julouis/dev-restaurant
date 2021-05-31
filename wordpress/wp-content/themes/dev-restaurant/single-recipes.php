af2eb87b1d3d16a9043dba9e6a824cc1036ae0d5
Julien Louis <julienlouis.becode@gmail.com>
2021-05-31 11:30:35 +0200

    update

--- wordpress/wp-content/themes/dev-restaurant/single-recipes.php
+++ wordpress/wp-content/themes/dev-restaurant/single-recipes.php
@@ -61,31 +61,140 @@ $recipeImage1Size = $recipeImage1['url'];
                 <img src="<?php echo $recipeImage1['url'] ?>" class="recipeImage1" alt="Responsive image">
 
 
+                <div class="row">
 
-                <h2 class="ingredientsText">Ingrédients</h2>
+                    <div class="col-2">
 
-                <p>For <? echo $recipeNumberOfPerson ?> people - preparation <?php echo $recipeCookingMinutes ?> minutes</p>
+                        <div class=" d-flex socialNetwork p-2 col flex-column align-items-sm-center  justify-content-xl-start align-items-xl-start  ">
+                            <!-- facebook -->
+                            <i class="fab fa-facebook-f d-flex justify-content-center align-items-center rounded-circle iconFooterSize m-1 bg-blue"></i>
 
-                <p class="recipeIngredient"><?php echo $recipeIngredient ?></p>
+                            <!-- twitter -->
+                            <div class=" ">
+                                <i class="fab fa-twitter d-flex justify-content-center align-items-center bg-info rounded-circle iconFooterSize m-1"></i>
+
+                            </div>
+
+                            <!-- instagram -->
+                            <div class="">
+                                <i class="fab fa-instagram d-flex justify-content-center align-items-center bg-danger rounded-circle iconFooterSize m-1 "></i>
+
+                            </div>
+
+                            <!-- linkedin -->
+                            <div class="">
+                                <i class="fab fa-linkedin-in d-flex justify-content-center align-items-center bg-primary rounded-circle iconFooterSize m-1 "></i>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-8">
+
+                        <h2 class="ingredientsText">Ingrédients</h2>
+
+                        <p>For <? echo $recipeNumberOfPerson ?> people - preparation <?php echo $recipeCookingMinutes ?> minutes</p>
+
+                        <p class="recipeIngredient"><?php echo $recipeIngredient ?></p>
+
+                    </div>
+
+                    <div class="col-2"></div>
+
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-2"></div>
+
+                    <div class="col-8">
+
+
+                        <h2 class="instructionsText">Instructions</h2>
+
+                        <p class="instructions_1"><?php echo $recipeInstructions1 ?></p>
+
+                        <p class="instructions_2"><?php echo $recipeInstructions2 ?></p>
+
+                        <img src="<?php echo $recipeImage2['url'] ?>" class="recipeImage2" alt="Responsive image">
+
+                        <p class="instructions_3"><?php echo $recipeInstructions3 ?></p>
+
+                        <p class="instructions_4"><?php echo $recipeInstructions4 ?></p>
+
+
+                    </div>
+
+                    <div class="col-2"></div>
+
+
+                </div>
 
 
 
-                <h2 class="instructionsText">Instructions</h2>
-
-                <p class="instructions_1"><?php echo $recipeInstructions1 ?></p>
-
-                <p class="instructions_2"><?php echo $recipeInstructions2 ?></p>
-
-                <img src="<?php echo $recipeImage2['url'] ?>" class="recipeImage2" alt="Responsive image">
-
-                <p class="instructions_3"><?php echo $recipeInstructions3 ?></p>
-
-                <p class="instructions_4"><?php echo $recipeInstructions4 ?></p>
 
             </div>
         </div>
         <div class="col-2"></div>
     </div>
+
+    <div class=" recipesMini d-flex flex-column align-items-center bg-light">
+        <img src="" alt="">
+        <div class="mt-5 text-center">
+            <h5>Latest updated</h5>
+            <h4>Recipes Blog</h4>
+        </div>
+
+        <div class="latestRecipe">
+            <div class="carMiniRecipe d-flex flex-wrap justify-content-center">
+                <?php
+                $the_query = new WP_Query(array(
+                    'category_name' => 'recipes',
+                    'orderby' => 'post_date',
+                    'order' => 'DESC',
+                    'posts_per_page' => '4'
+                ));
+
+                if ($the_query->have_posts()) :
+                    while ($the_query->have_posts()) :
+                        $the_query->the_post();
+                ?>
+                        <div class=" recipeCard">
+
+                            <div class=" recipeSingle card m-3">
+                                <div class="imgRecipeCard">
+                                    <?php the_post_thumbnail(array(300, 500)) ?>
+                                </div>
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p class="card-text ">
+                                            <small class="text-muted">
+                                                <i class="fas fa-clock me-2 "></i>
+                                                Last updated 3 mins ago
+                                            </small>
+                                        </p>
+                                        <h3><?php the_title(); ?></h3>
+                                        <?php the_excerpt(); ?>
+                                    </div>
+                                    <div class="d-flex justify-content-around">
+                                        <p class="border"></p>
+                                        <hr class="border border-white flex-grow-1 ">
+                                        <a href="<?php the_permalink() ?>" role="button" class="fw-bold text-uppercase text-dark"> Read More </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                <?php endwhile;
+                endif; ?>
+
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
